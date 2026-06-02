b1e814ce03988a7c9a17d4f7d4de7425e0f611aa
Mohammedleadmagnetd <m.marwan2003@gmail.com>
2026-06-02 23:43:27 +0200

    fix Instagram scope to public_profile only

--- app/api/auth/instagram/route.js
+++ app/api/auth/instagram/route.js
@@ -3,10 +3,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const appId = process.env.INSTAGRAM_APP_ID;
   const redirectUri = "https://leadmagnetinc.com/api/auth/instagram/callback";
-  const scope = "email,public_profile";
   const state = Math.random().toString(36).substring(7);
 
-  const url = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&state=${state}&response_type=code`;
+  const url = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=public_profile&state=${state}&response_type=code`;
 
   return NextResponse.redirect(url);
 }
